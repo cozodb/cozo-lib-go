7cc1eb0a7f8e801b2d6523324d2b94562a3f533e
Ziyang Hu <hu.ziyang@cantab.net>
2022-11-29 21:24:13 +0800

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module cozo
+
+go 1.19
+
+require github.com/stretchr/objx v0.5.0
---
+++ main.go
@@ -0,0 +1,348 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/stretchr/objx"
+	"log"
+	"unsafe"
+)
+
+/*
+#include <string.h>
+#include "cozo_c.h"
+#cgo LDFLAGS: -L${SRCDIR} -lcozo_c -lstdc++ -lsqlite3
+#cgo darwin LDFLAGS: -framework Security
+*/
+import "C"
+
+type CozoDB struct {
+	Id C.int32_t
+}
+
+var emptyMap = C.CString("{}")
+
+type Map = objx.Map
+
+type QueryError struct {
+	Data Map
+}
+
+type NamedRows struct {
+	Headers []string `json:"headers"`
+	Rows    [][]any  `json:"rows"`
+	Took    float64  `json:"took"`
+	Ok      bool     `json:"ok"`
+}
+
+func (m QueryError) Error() string {
+	msg := ""
+	msg = m.Data.Get("display").String()
+	if len(msg) > 0 {
+		return msg
+	}
+
+	msg = m.Data.Get("message").String()
+	if len(msg) > 0 {
+		return msg
+	}
+	return "Unknown error"
+}
+
+func New(engine string, path string, options Map) (CozoDB, error) {
+	var ret CozoDB
+	cEngine := C.CString(engine)
+	defer C.free(unsafe.Pointer(cEngine))
+
+	cPath := C.CString(path)
+	defer C.free(unsafe.Pointer(cPath))
+
+	var cOptions *C.char
+	if options == nil {
+		cOptions = emptyMap
+	} else {
+		b, err := json.Marshal(options)
+		cOptions = C.CString(string(b))
+		defer C.free(unsafe.Pointer(cOptions))
+		if err != nil {
+			return ret, err
+		}
+	}
+
+	err := C.cozo_open_db(cEngine, cPath, cOptions, &ret.Id)
+	if err != nil {
+		goErr := C.GoString(err)
+		C.cozo_free_str(err)
+		return ret, errors.New(goErr)
+	}
+	return ret, nil
+}
+
+func (db *CozoDB) Close() {
+	C.cozo_close_db(db.Id)
+}
+
+func (db *CozoDB) Run(query string, params Map) (NamedRows, error) {
+	var paramsStr *C.char
+	result := NamedRows{
+		Ok:   false,
+		Took: -1.0,
+	}
+
+	if params == nil {
+		paramsStr = emptyMap
+	} else {
+		b, err := json.Marshal(params)
+		paramsStr = C.CString(string(b))
+		defer C.free(unsafe.Pointer(paramsStr))
+
+		if err != nil {
+			return result, err
+		}
+	}
+
+	queryStr := C.CString(query)
+	defer C.free(unsafe.Pointer(queryStr))
+
+	res := C.cozo_run_query(db.Id, queryStr, paramsStr)
+	defer C.cozo_free_str(res)
+
+	cLen := C.int(C.strlen(res))
+	gBytes := C.GoBytes(unsafe.Pointer(res), cLen)
+
+	jsonErr := json.Unmarshal(gBytes, &result)
+	if jsonErr != nil {
+		return result, jsonErr
+	}
+
+	if result.Ok {
+		return result, nil
+	} else {
+		var raw Map
+
+		jsonErr := json.Unmarshal(gBytes, &raw)
+		if jsonErr != nil {
+			return result, jsonErr
+		}
+
+		return result, QueryError{
+			Data: raw,
+		}
+	}
+}
+
+func (db *CozoDB) ImportRelations(payload Map) error {
+	b, err := json.Marshal(payload)
+	if err != nil {
+		return err
+	}
+
+	cPayload := C.CString(string(b))
+	defer C.free(unsafe.Pointer(cPayload))
+
+	var res Map
+	cRes := C.cozo_import_relations(db.Id, cPayload)
+	defer C.cozo_free_str(cRes)
+	cLen := C.int(C.strlen(cRes))
+	gBytes := C.GoBytes(unsafe.Pointer(cRes), cLen)
+
+	jsonErr := json.Unmarshal(gBytes, &res)
+	if jsonErr != nil {
+		return jsonErr
+	}
+	if !res.Get("ok").Bool(false) {
+		return QueryError{
+			Data: res,
+		}
+	} else {
+		return nil
+	}
+}
+
+func (db *CozoDB) ExportRelations(relations []string) (Map, error) {
+	payloadMap := map[string][]string{
+		"relations": relations,
+	}
+
+	b, err := json.Marshal(payloadMap)
+	if err != nil {
+		return nil, err
+	}
+
+	payload := C.CString(string(b))
+	defer C.free(unsafe.Pointer(payload))
+
+	var res Map
+	cRes := C.cozo_export_relations(db.Id, payload)
+	defer C.cozo_free_str(cRes)
+	cLen := C.int(C.strlen(cRes))
+	gBytes := C.GoBytes(unsafe.Pointer(cRes), cLen)
+
+	jsonErr := json.Unmarshal(gBytes, &res)
+	if jsonErr != nil {
+		return nil, jsonErr
+	}
+	if !res.Get("ok").Bool(false) {
+		return nil, QueryError{
+			Data: res,
+		}
+	} else {
+		return res.Get("data").MustObjxMap(), nil
+	}
+}
+
+func (db *CozoDB) Backup(path string) error {
+	cPath := C.CString(path)
+	defer C.free(unsafe.Pointer(cPath))
+	cRes := C.cozo_backup(db.Id, cPath)
+	defer C.cozo_free_str(cRes)
+
+	cLen := C.int(C.strlen(cRes))
+	gBytes := C.GoBytes(unsafe.Pointer(cRes), cLen)
+
+	var res Map
+	jsonErr := json.Unmarshal(gBytes, &res)
+	if jsonErr != nil {
+		return jsonErr
+	}
+	if !res.Get("ok").Bool(false) {
+		return QueryError{
+			Data: res,
+		}
+	} else {
+		return nil
+	}
+}
+
+func (db *CozoDB) Restore(path string) error {
+	cPath := C.CString(path)
+	defer C.free(unsafe.Pointer(cPath))
+	cRes := C.cozo_restore(db.Id, cPath)
+	defer C.cozo_free_str(cRes)
+
+	cLen := C.int(C.strlen(cRes))
+	gBytes := C.GoBytes(unsafe.Pointer(cRes), cLen)
+
+	var res Map
+	jsonErr := json.Unmarshal(gBytes, &res)
+	if jsonErr != nil {
+		return jsonErr
+	}
+	if !res.Get("ok").Bool(false) {
+		return QueryError{
+			Data: res,
+		}
+	} else {
+		return nil
+	}
+}
+
+func (db *CozoDB) ImportRelationsFromBackup(path string, relations []string) error {
+	payloadMap := map[string]any{
+		"relations": relations,
+		"path":      path,
+	}
+
+	b, err := json.Marshal(payloadMap)
+	if err != nil {
+		return err
+	}
+
+	payload := C.CString(string(b))
+	defer C.free(unsafe.Pointer(payload))
+
+	var res Map
+	cRes := C.cozo_import_from_backup(db.Id, payload)
+	defer C.cozo_free_str(cRes)
+	cLen := C.int(C.strlen(cRes))
+	gBytes := C.GoBytes(unsafe.Pointer(cRes), cLen)
+
+	jsonErr := json.Unmarshal(gBytes, &res)
+	if jsonErr != nil {
+		return jsonErr
+	}
+	if !res.Get("ok").Bool(false) {
+		return QueryError{
+			Data: res,
+		}
+	} else {
+		return nil
+	}
+}
+
+func main() {
+	db, err := New("mem", "", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, _ = db.Run("?[a,b,c] <- [[1,2,3]] :create s{a, b, c}", nil)
+	{
+		res, _ := db.Run("?[a,b,c] := *s[a,b,c]", nil)
+		fmt.Println(res)
+	}
+
+	{
+		_, err := db.Run("?[x] <- [[1,2,3]]", nil)
+		fmt.Println(err)
+	}
+
+	{
+		err := db.Backup("test.db")
+		fmt.Println(err)
+
+		db2, err := New("mem", "", nil)
+		_ = db2.Restore("test.db")
+
+		res, err := db2.Run("?[a,b,c] := *s[a,b,c]", nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(res)
+	}
+
+	{
+		data, err := db.ExportRelations([]string{"s"})
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(data)
+		db3, _ := New("mem", "", nil)
+		_, _ = db3.Run(":create s {a, b, c}", nil)
+
+		res, err := db3.Run("?[a,b,c] := *s[a,b,c]", nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("db3!")
+		fmt.Println(res)
+		_ = db3.ImportRelations(data)
+
+		res, err = db3.Run("?[a,b,c] := *s[a,b,c]", nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("db3 imported!")
+		fmt.Println(res)
+
+		db4, _ := New("mem", "", nil)
+		_, _ = db4.Run(":create s {a, b, c}", nil)
+
+		res, err = db4.Run("?[a,b,c] := *s[a,b,c]", nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("db4!")
+		fmt.Println(res)
+		_ = db4.ImportRelationsFromBackup("test.db", []string{"s"})
+
+		res, err = db4.Run("?[a,b,c] := *s[a,b,c]", nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("db4 imported!")
+		fmt.Println(res)
+	}
+
+	db.Close()
+}
